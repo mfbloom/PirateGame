d2a756820b42c0a082a0cd745663d1e6893ffd87
Michael Bloom <michaelbloom@macbookpro.local>
2014-05-30 18:46:29 -0400

    first commit

--- PirateGame/MBViewController.h
+++ PirateGame/MBViewController.h
@@ -7,7 +7,39 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MBCharacter.h"
+#import "MBBoss.h"
 
 @interface MBViewController : UIViewController
+
+//iVars
+@property (nonatomic) CGPoint currentPoint;
+@property (strong, nonatomic) NSArray *tiles;
+@property (strong, nonatomic) MBCharacter *character;
+@property (strong, nonatomic) MBBoss *boss;
+
+//IB Outlets
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImage;
+@property (strong, nonatomic) IBOutlet UILabel *healthLabel;
+@property (strong, nonatomic) IBOutlet UILabel *damageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weaponLabel;
+@property (strong, nonatomic) IBOutlet UILabel *armorLabel;
+@property (strong, nonatomic) IBOutlet UILabel *storyLabel;
+@property (strong, nonatomic) IBOutlet UIButton *actionButton;
+@property (strong, nonatomic) IBOutlet UIButton *northButton;
+@property (strong, nonatomic) IBOutlet UIButton *westButton;
+@property (strong, nonatomic) IBOutlet UIButton *eastButton;
+@property (strong, nonatomic) IBOutlet UIButton *southButton;
+@property (strong, nonatomic) IBOutlet UIButton *resetGameButton;
+
+
+//Actions - methods automatically added to when created here
+- (IBAction)actionButtonPressed:(UIButton *)sender;
+- (IBAction)northButtonPressed:(UIButton *)sender;
+- (IBAction)westButtonPressed:(UIButton *)sender;
+- (IBAction)eastButtonPressed:(UIButton *)sender;
+- (IBAction)southButtonPressed:(UIButton *)sender;
+- (IBAction)resetGamePressed:(UIButton *)sender;
+
 
 @end
