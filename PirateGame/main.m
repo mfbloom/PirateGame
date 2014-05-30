aa502c6554f83504757ae3178395a632bd213785
Michael Bloom <michaelbloom@macbookpro.local>
2014-05-29 22:00:47 -0400

    Initial Commit

---
+++ PirateGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PirateGame
+//
+//  Created by Michael Bloom on 5/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
